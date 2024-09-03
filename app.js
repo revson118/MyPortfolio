ceb5b72d54290549bff0a55f4c2465cd3daed85d
Hrithik Rudra <112173833+revson118@users.noreply.github.com>
2024-09-03 11:51:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+document.getElementById('post').addEventListener('click',function(){
+    window.location.href="https://www.facebook.com/hrithik.rudra.7";
+})
+document.getElementById('cost').addEventListener('click',function(){
+    window.location.href="https://www.linkedin.com/in/hrithik-rudra-3b7605239/"
+})
+
+document.getElementById("git").addEventListener('click',function(){
+    window.location.href="https://github.com/revson118"
+})
+
+document.getElementById("app1").addEventListener('click',function(){
+  window.location.href="http://127.0.0.1:5500/index1.html"
+})
+
+document.getElementById("app2").addEventListener('click',function(){
+  window.location.href="http://127.0.0.1:5500/edu.html"
+})
+
+document.getElementById("app3").addEventListener('click',function(){
+  window.location.href="http://127.0.0.1:5500/contact.html"
+})
+document.getElementById("app4").addEventListener('click',function(){
+  window.location.href="http://127.0.0.1:5500/projects.html"
+})
